a9ae6b2c21b41addb2911503664d1bd1f304f2e5
mapucat <mariapaularoes@gmail.com>
2024-07-08 14:29:56 -0500

    [FA-1] Update icons svg and add menu element

--- src/components/ui/Icon.tsx
+++ src/components/ui/Icon.tsx
@@ -1,11 +1,13 @@
+import React from 'react';
 import { ReactComponent as Search } from './../../assets/svg/search.svg';
 import { ReactComponent as NoData } from './../../assets/svg/no_data.svg';
 import { ReactComponent as Clear } from './../../assets/svg/x.svg';
 import { ReactComponent as Save } from './../../assets/svg/save.svg';
 import { ReactComponent as SaveFilled } from './../../assets/svg/save-filled.svg';
+import { ReactComponent as Menu } from './../../assets/svg/menu.svg';
 
 type IconProps = {
-  name: 'search' | 'noData' | 'clear' | 'save' | 'saveFilled';
+  name: 'search' | 'noData' | 'clear' | 'save' | 'saveFilled' | 'menu';
 };
 
 const Icon = (props: IconProps) => {
@@ -17,6 +19,7 @@ const Icon = (props: IconProps) => {
     clear: <Clear />,
     save: <Save />,
     saveFilled: <SaveFilled />,
+    menu: <Menu />,
   };
 
   return Icons[name];
